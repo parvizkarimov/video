d4b236b4628830cb1c3990c863f835f06acb3aba
Parviz <parviz0102@gmail.com>
2026-08-13 22:25:00 +0500

    Remove website directory to force Python build

--- website/composables/useBrandHighlight.ts
+++
@@ -1,7 +0,0 @@
-const BRAND_SPAN = '<span class="text-[#7C3AED] font-bold">klangbild</span>';
-
-export const useBrandHighlight = () => {
-  const highlight = (text: string) => text.replace(/klangbild/g, BRAND_SPAN);
-
-  return { highlight };
-};
